130bd8cc02ff0c4d2c7a5e3c50e0f2fa645f6252
jtheitkamp19 <93356798+jtheitkamp19@users.noreply.github.com>
2021-11-17 17:28:16 -0500

    Initial Commit

---
+++ pokemon-services/src/main/java/com/uneventomcat/pokemonservices/PokemonServicesApplication.java
@@ -0,0 +1,13 @@
+package com.uneventomcat.pokemonservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonServicesApplication.class, args);
+	}
+
+}
---
+++ pokemon-services/src/test/java/com/uneventomcat/pokemonservices/PokemonServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uneventomcat.pokemonservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
