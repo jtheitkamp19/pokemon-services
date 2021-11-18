a70381a05404e31b1f7e1af3d642f6f00b99fef5
Jordan Heitkamp <j-heitkamp@onu.edu>
2021-11-18 12:15:24 -0500

    Adds Persistence Configuration

---
+++ src/main/java/com/uneventomcat/pokemonservices/config/PersistenceConfiguration.java
@@ -0,0 +1,23 @@
+package com.uneventomcat.pokemonservices.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+    @Value("${DB_URL}")
+    private String databaseURL;
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url(databaseURL);
+
+        System.out.println("Custom Datasource set");
+        return builder.build();
+    }
+}
