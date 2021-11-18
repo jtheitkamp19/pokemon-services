fef1170aedabb78639f2b8e07c5291242c1e5a15
Jordan Heitkamp <j-heitkamp@onu.edu>
2021-11-18 13:15:59 -0500

    Makes changes to more closely match expected

--- src/main/java/com/uneventomcat/pokemonservices/models/Pokemon.java
+++ src/main/java/com/uneventomcat/pokemonservices/models/Pokemon.java
@@ -1,9 +1,12 @@
 package com.uneventomcat.pokemonservices.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.List;
 
-@Entity
+@Entity(name = "pokemons")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Pokemon {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/uneventomcat/pokemonservices/models/Type.java
+++ src/main/java/com/uneventomcat/pokemonservices/models/Type.java
@@ -1,11 +1,13 @@
 package com.uneventomcat.pokemonservices.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import java.util.List;
 
-@Entity
+@Entity(name = "types")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Type {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
