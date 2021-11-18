bc41dfbe19ad97f668e3e24885b4ed6a34ade111
Jordan Heitkamp <j-heitkamp@onu.edu>
2021-11-18 13:50:15 -0500

    Updates Models

--- src/main/java/com/uneventomcat/pokemonservices/models/Type.java
+++ src/main/java/com/uneventomcat/pokemonservices/models/Type.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import java.util.List;
 
-@Entity(name = "types")
+@Entity(name = "type")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Type {
     @Id
