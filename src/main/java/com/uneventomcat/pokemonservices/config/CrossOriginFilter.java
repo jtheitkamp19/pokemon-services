45781f09e567f2731ee3519e4e9bed9003e52893
Jordan Heitkamp <j-heitkamp@onu.edu>
2021-11-25 19:19:53 -0500

    Adds cross origin header

---
+++ src/main/java/com/uneventomcat/pokemonservices/config/CrossOriginFilter.java
@@ -0,0 +1,26 @@
+package com.uneventomcat.pokemonservices.config;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CrossOriginFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "https://jtheitkamp19.github.io/");
+        filterChain.doFilter(servletRequest, httpServletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
