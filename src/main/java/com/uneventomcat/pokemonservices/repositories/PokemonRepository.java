c69e68849a3e7626411dbffb3d9589a7c08c64c0
Jordan Heitkamp <j-heitkamp@onu.edu>
2021-11-29 10:35:05 -0500

    Orders return result for family retrieval

--- src/main/java/com/uneventomcat/pokemonservices/repositories/PokemonRepository.java
+++ src/main/java/com/uneventomcat/pokemonservices/repositories/PokemonRepository.java
@@ -11,6 +11,6 @@ public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
 
     List<Pokemon> findByNameContaining(String name);
 
-    @Query(value = "SELECT * FROM pokemon WHERE familyid = (SELECT familyid FROM pokemon WHERE number = :id);", nativeQuery = true)
+    @Query(value = "SELECT * FROM pokemon WHERE familyid = (SELECT familyid FROM pokemon WHERE number = :id) ORDER BY number ASC;", nativeQuery = true)
     List<Pokemon> findPokeFamilyForPokemonWithId(@Param("id") Long id);
 }
